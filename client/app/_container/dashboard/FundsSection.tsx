b4b0bba3278993fce6c3f6c2bacfe946fcfb4d25
L-Pircy <taveesha.agarwal@gmail.com>
2024-02-17 21:10:26 +0530

    minor des changes in dashboard

--- client/app/_container/dashboard/FundsSection.tsx
+++ client/app/_container/dashboard/FundsSection.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 const FundsSection = () => {
   return (
-    <div className="flex flex-col gap-4 self-start place-self-end p-4">
+    <div className="flex flex-col gap-4 self-start place-self-end p-4 max-h-[100%] items-start justify-start">
       <TotalFunds userFunds={userFunds} />
       {/* chain number */}
       <div className="w-[100%] border-[0.9px] border-[#e2c02a] bg-[#FECE00] font-normal rounded-[16px] px-4 py-[12px] grid grid-flow-col gap-4 h-fit">
@@ -21,7 +21,7 @@ const FundsSection = () => {
         </h3>
       </div>
       {/* chains and their funds */}
-      <div className="w-[288px] grid gap-[16px] overflow-scroll max-h-[413px]">
+      <div className="w-[288px] grid gap-[16px] overflow-scroll max-h-[330px]">
         {chains.map((chain) => (
           <ChainFund chain={chain.name} bgCol={chain.bgProp} key={chain.name} />
         ))}
